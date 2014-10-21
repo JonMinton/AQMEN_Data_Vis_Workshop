ecc5616fe4154db99640802f06ecc7336e219baf
Jonathan Minton <nate.minton@gmail.com>
2014-10-21 15:27:17 +0100

    fix merge conflict and finish jsa time series tidying

--- data/contributed/konrad/tidy_the_data_2.r
+++ data/contributed/konrad/tidy_the_data_2.r
@@ -99,6 +99,10 @@ ds_1$variable <- revalue(
 ds_1_tidy <- dcast(ds_1, id.var=c("datazone", "year"), ... ~ variable)
 ds_1_tidy <- arrange(ds_1_tidy, year, datazone)
 
+ds_1_tidy[,-1] <- apply(ds_1_tidy[,-1], 2, as.numeric)
+
+
+###############################################################
 # Next example
 
 jsa_vars <- str_detect(
@@ -116,27 +120,8 @@ ds_jsa <- subset(
            )
   )
 
-<<<<<<< HEAD:data/contributed/konrad/tidy_the_data_2.r
-###
-> unique(ds_1_var_4$variable)
-[1] "crsimdcrime_totrat" "edsldf_other_p_"    "edsldf_positive_p_" "crsimdcrime_totno" 
-[5] "simd" 
-
-
-
-
-var_2 <- c(
-  "simd12",  
-  "simd09",	
-  "edsqass4avgts1011",	
-  "dec_simd12",	
-  "q_simd12"
-=======
-ds_jsa <- melt(
-  ds_jsa,
-  id.var=c("datazone")
->>>>>>> 85f8fdf6cde3e15c0af3f6ea745201cbe9d06637:data/contributed/konrad/tidy_the_data.r
-  )
+ds_jsa <- melt(ds_jsa, id.var=c("datazone"))
+ds_jsa$value <- as.numeric(ds_jsa$value)
 
 # In all cases, the date information is contained in the section of the name from the 
 # _ character to the end of the string
@@ -146,152 +131,65 @@ ds_jsa$month_year <- str_match(
   "[^_]*?$"
   )
 
+
 # year only
 
 ds_jsa$year <- str_sub(ds_jsa$variable, str_length(ds_jsa$variable) - 1)
 ds_jsa$year <- as.numeric(ds_jsa$year)
 
 is_99 <- which(ds_jsa$year==99)
-ds_jsa$year[is_99] <- ds_jsa$year[is_99] + 1900
-ds_jsa$year[!is_99] <- ds_jsa$year[!is_99] + 2000
+ds_jsa$year <- ds_jsa$year + 2000
+ds_jsa$year[is_99] <- ds_jsa$year[is_99] - 100
 
-# D2
-# no_total_jsa_august99  
-# no_total_jsa_november99	
-# no_total_jsa_february00	
-# no_total_jsa_may00	
-# no_total_jsa_august00	
-# no_total_jsa_november00	
-# no_total_jsa_february01	
-# no_total_jsa_may01	
-# no_total_jsa_august01	
-# no_total_jsa_november01	
-# no_total_jsa_february02	
-# no_total_jsa_may02	
-# no_total_jsa_august02	
-# no_total_jsa_november02	
-# no_total_jsa_february03	
-# no_total_jsa_may03	
-# no_total_jsa_august03	
-# no_total_jsa_november03	
-# no_total_jsa_february04	
-# no_total_jsa_may04	
-# no_total_jsa_august04	
-# no_total_jsa_november04	
-# no_total_jsa_february05	
-# no_total_jsa_may05	
-# no_total_jsa_august05	
-# no_total_jsa_november05	
-# no_total_jsa_february06	
-# no_total_jsa_may06	
-# no_total_jsa_august06	
-# no_total_jsa_november06	
-# no_total_jsa_february07	
-# no_total_jsa_may07	
-# no_total_jsa_august07	
-# no_total_jsa_november07	
-# no_total_jsa_february08	
-# no_total_jsa_may08	
-# no_total_jsa_august08	
-# no_total_jsa_november08	
-# no_total_jsa_february09	
-# no_total_jsa_may09	
-# no_total_jsa_august09	
-# no_total_jsa_november09	
-# no_total_jsa_february10	
-# no_total_jsa_may10	
-# no_total_jsa_august10	
-# no_total_jsa_november10	
-# no_total_jsa_february11	
-# no_total_jsa_may11	
-# no_total_jsa_august11	
-# no_total_jsa_november11	
-# no_total_jsa_february12	
-# no_total_jsa_may12	
-# no_total_jsa_august12	
-# no_total_jsa_november12	
-# no_total_jsa_february13	
-# no_total_jsa_may13	
-# no_total_jsa_august13	
-# no_total_jsa_november13	
-# no_total_jsa_1624_august1999	
-# no_total_jsa_1624_november1999	
-# no_total_jsa_1624_february2000	
-# no_total_jsa_1624_may2000	
-# no_total_jsa_1624_august2000	
-# no_total_jsa_1624_november2000
-# no_total_jsa_1624_february2001	
-# no_total_jsa_1624_may2001	
-# no_total_jsa_1624_august2001	
-# no_total_jsa_1624_november2001	
-# no_total_jsa_1624_february2002	
-# no_total_jsa_1624_may2002	
-# no_total_jsa_1624_august2002	
-# no_total_jsa_1624_november2002	
-# no_total_jsa_1624_february2003	
-# no_total_jsa_1624_may2003	
-# no_total_jsa_1624_august2003	
-# no_total_jsa_1624_november2003	
-# no_total_jsa_1624_february2004	
-# no_total_jsa_1624_may2004	
-# no_total_jsa_1624_august2004	
-# no_total_jsa_1624_november2004	
-# no_total_jsa_1624_february2005	
-# no_total_jsa_1624_may2005	
-# no_total_jsa_1624_august2005	
-# no_total_jsa_1624_november2005	
-# no_total_jsa_1624_february2006	
-# no_total_jsa_1624_may2006	
-# no_total_jsa_1624_august2006	
-# no_total_jsa_1624_november2006	
-# no_total_jsa_1624_february2007	
-# no_total_jsa_1624_may2007	
-# no_total_jsa_1624_august2007	
-# no_total_jsa_1624_november2007	
-# no_total_jsa_1624_february2008	
-# no_total_jsa_1624_may2008	
-# no_total_jsa_1624_august2008	
-# no_total_jsa_1624_november2008	
-# no_total_jsa_1624_february2009	
-# no_total_jsa_1624_may2009	
-# no_total_jsa_1624_august2009	
-# no_total_jsa_1624_november2009	
-# no_total_jsa_1624_february2010	
-# no_total_jsa_1624_may2010	
-# no_total_jsa_1624_august2010	
-# no_total_jsa_1624_november2010	
-# no_total_jsa_1624_february2011	
-# no_total_jsa_1624_may2011	
-# no_total_jsa_1624_august2011	
-# no_total_jsa_1624_november2011	
-# no_total_jsa_1624_february2012	
-# no_total_jsa_1624_may2012	
-# no_total_jsa_1624_august2012	
-# no_total_jsa_1624_november2012	
-# no_total_jsa_1624_february2013	
-# no_total_jsa_1624_may2013	
-# no_total_jsa_1624_august2013	
-# no_total_jsa_1624_november2013	
-# cs_jsa_pertotal_2001q01	
-# cs_jsa_pertotal_2001q02	
-# cs_jsa_pertotal_2001q03	
-# cs_jsa_pertotal_2001q04	
-# cs_jsa_pertotal_2002q01	
-# cs_jsa_pertotal_2002q02	
-# cs_jsa_pertotal_2002q03	
-# cs_jsa_pertotal_2002q04	
-# cs_jsa_pertotal_2003q01	
-# cs_jsa_pertotal_2003q02	
-# cs_jsa_pertotal_2003q03	
-# cs_jsa_pertotal_2003q04	
-# cs_jsa_pertotal_2004q01	
-# cs_jsa_pertotal_2004q02	
-# cs_jsa_pertotal_2004q03	
-# cs_jsa_pertotal_2004q04	
-# cs_jsa_pertotal_2005q01	
-# cs_jsa_pertotal_2005q02	
-# cs_jsa_pertotal_2005q03	
-# cs_jsa_pertotal_2005q04
+ds_jsa$month <- str_sub(ds_jsa$month_year, 1, str_length(ds_jsa$month_year)-2)
 
+unique(ds_jsa$month)
+
+ds_jsa$month <- str_replace(ds_jsa$month, "[0-9]{1,2}", "")
+
+ds_jsa$quarter <- revalue(
+  ds_jsa$month,
+    c(
+      "february"=1,
+      "may"=2,
+      "july" =3,
+      "august"=3,
+      "november"=4
+      )
+  )
+
+ds_jsa$quarter <- as.numeric(ds_jsa$quarter)
+
+ds_jsa$yearpart <- revalue(
+  ds_jsa$month,
+   c(
+     "february"=1.5/12,
+     "may"=4.5/12,
+     "july" =6.5/12,
+     "august"=7.5/12,
+     "november"=10.5/12     
+     )                               
+  )
+
+ds_jsa$year <- as.numeric(ds_jsa$year)
+ds_jsa$yearpart <- as.numeric(ds_jsa$yearpart)
+ds_jsa$year_combined <- ds_jsa$year + ds_jsa$yearpart
+
+ds_jsa$month_year <- NULL
+ds_jsa$yearpart <- NULL
+ds_jsa$variable <- "jsa_claimant_count"
+ds_jsa$value <- as.numeric(ds_jsa$value)
+ds_jsa$month <- NULL
+ds_jsa$quarter <- NULL
+ds_jsa$year <- NULL
+ds_jsa_backup <- ds_jsa
+ds_jsa <- dcast(
+  ds_jsa,  
+  formula= ... ~ variable,
+  id= c("datazone",  "year_combined"),
+  mean
+  )
+
+ds_jsa <- arrange(ds_jsa, year_combined, datazone)
 
 
